fa871375c80a0fbc4588cbd8bf1ce221d4f517e6
sadioraymond <sadiowalyraymond@gmail.com>
2018-02-20 10:08:01 +0000

    Adding design

--- app/component/Main.js
+++ app/component/Main.js
@@ -10,20 +10,91 @@ import {
 } from 'react-native';
 
 export default class Main extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            noteArray: [],
+            tachText: '',
+        };
+    }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Tester</Text>
-      </View>
+        <View style={styles.container}>
+        <View style={styles.header}>
+            <Text style={styles.headerText}>- Liste Des Taches -</Text>
+        </View>
+        <ScrollView style={styles.scrollContainer}>
+           
+        </ScrollView>
+
+        <View style={styles.footer}>
+            <TextInput 
+                style={styles.textInput}
+                placeholder='>tache'
+                onChangeText={(tacheText)=> this.setState({tacheText})}
+                value={this.state.tacheText}
+                placeholderTextColor='white'
+                underlineColorAndroid='transparent'>
+            </TextInput>
+        </View>
+        <TouchableOpacity style={styles.addButton}>
+            <Text style={styles.addButtonText}>+</Text>
+        </TouchableOpacity>
+    </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+    },
+    header: {
+        backgroundColor: '#E91E63',
+        alignItems: 'center',
+        justifyContent:'center',
+        borderBottomWidth: 10,
+        borderBottomColor: '#ddd'
+    },
+    headerText: {
+        color: 'white',
+        fontSize: 18,
+        padding: 26
+    },
+    scrollContainer: {
+        flex: 1,
+        marginBottom: 100
+    },
+    footer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: 10
+    },
+    textInput: {
+        alignSelf: 'stretch',
+        color: '#fff',
+        padding: 20,
+        backgroundColor: '#252525',
+        borderTopWidth:2,
+        borderTopColor: '#ededed'
+    },
+    addButton: {
+        position: 'absolute',
+        zIndex: 11,
+        right: 20,
+        bottom: 90,
+        backgroundColor: '#E91E63',
+        width: 70,
+        height: 70,
+        borderRadius: 35,
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 8
+    },
+    addButtonText: {
+        color: '#fff',
+        fontSize: 24
+    }
 });
